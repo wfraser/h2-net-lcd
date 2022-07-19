16a867d548c107448864c67768d9457b868be742
William R. Fraser <wfraser@codewise.org>
2022-07-19 11:58:44 -0700

    panic on errors during init, but not afterwards

--- src/lcd_display.rs
+++ src/lcd_display.rs
@@ -8,17 +8,37 @@ use lcd::{
     FunctionLine,
 };
 use lcd_pcf8574::{Pcf8574, ErrorHandling};
+use std::cell::Cell;
+use std::rc::Rc;
 
-const I2C_BUS: u8 = 2;
+const I2C_BUS: u8 = 1;
 const I2C_ADDR: u16 = 0x27;
 
 pub fn init_display() -> Result<Display<Pcf8574>> {
+
     let mut dev = Pcf8574::new(I2C_BUS, I2C_ADDR)
         .context("failed to open I2C device")?;
-    dev.on_error(ErrorHandling::Custom(Box::new(|e| eprintln!("I/O error: {}", e))));
+
+    // Fail fast and panic on errors during init, so we can quickly know if the parameters are
+    // wrong.
+    let epanic = Rc::new(Cell::new(true));
+    dev.on_error(ErrorHandling::Custom(Box::new({
+        let epanic = Rc::clone(&epanic);
+        move |e| {
+            if epanic.get() {
+                panic!("I/O error: {}", e);
+            } else {
+                eprintln!("I/O error: {}", e);
+            }
+        }
+    })));
 
     let mut display = Display::new(dev);
     display.init(FunctionLine::Line2, FunctionDots::Dots5x8);
+
+    // If it successfully init'd, we're probably good to just print errors now.
+    epanic.set(false);
+
     display.display(
         DisplayMode::DisplayOn,
         DisplayCursor::CursorOff,
